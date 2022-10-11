2cd835db6c562344b84239456fd257517a584152
Taehwan Kim <obtusa07@gmail.com>
2022-10-11 20:36:10 +0900

    :lipstick: CollectionView 들어가기전 사전 작업 완료
    1. 해당 부분에서 바탕화면에 깔릴 이미지를 넣었습니다.
    2. Lables, UIView 작업 완료

--- Vincent/Screens/Home/HomeViewController.swift
+++ Vincent/Screens/Home/HomeViewController.swift
@@ -7,7 +7,20 @@
 
 import UIKit
 
-final class HomeViewController: BaseViewController {
+struct Data {
+    let memberName = ["효정", "미미", "유아", "승희", "지호", "비니", "아린", "효정", "미미", "유아", "승희", "지호", "비니", "아린", "효정", "미미", "유아", "승희", "지호", "비니", "아린", "효정", "미미", "유아", "승희", "지호", "비니", "아린", "효정", "미미", "유아", "승희", "지호", "비니", "아린", "효정", "미미", "유아", "승희", "지호", "비니", "아린"]
+}
+final class HomeViewController: BaseViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    private var collectionView = {
+        var cv = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewLayout())
+        cv.backgroundColor = .gray
+        cv.register(HomeCell.self, forCellWithReuseIdentifier: HomeCell.cellID)
+        return cv
+    }()
+    private let wallPaper = UIImageView().then {
+        $0.image = UIImage(named: "wallpaperHome")
+        $0.contentMode = .scaleAspectFill
+    }
     private let helloLabel = UILabel().then {
         $0.text = "Hello,"
         $0.textColor = .gray
@@ -25,26 +38,62 @@ final class HomeViewController: BaseViewController {
         $0.clipsToBounds = true
     }
     private let searchBar = UIView().then {
-        $0.backgroundColor = .orange
+        $0.backgroundColor = .lightGray
         $0.layer.cornerRadius = 10
     }
+    
     override func render() {
-        view.addSubviews(helloLabel, vincentLabel, profileImage)
+        view.addSubviews(wallPaper, helloLabel, vincentLabel, profileImage, collectionView, searchBar)
+//        collectionView.snp.makeConstraints {
+//            $0.top.equalTo(vincentLabel.snp.bottom).offset(30)
+//            $0.leading.equalTo(view.safeAreaLayoutGuide.snp.leading).offset(0)
+//            $0.trailing.equalTo(view.safeAreaLayoutGuide.snp.trailing).offset(0)
+//            $0.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom).offset(30)
+//            //      $0.edges.equalToSuperview().inset(UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0))
+//        }
+        wallPaper.snp.makeConstraints {
+            $0.edges.equalToSuperview().inset(UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0))
+        }
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
         helloLabel.snp.makeConstraints {
-            $0.leading.equalTo(20)
-            $0.top.equalTo(30)
+            $0.leading.equalTo(view.safeAreaLayoutGuide.snp.leading).offset(20)
+            $0.top.equalTo(self.view.safeAreaLayoutGuide.snp.top).offset(30)
         }
         vincentLabel.snp.makeConstraints {
-            $0.leading.equalTo(20)
-            $0.top.equalTo(60)
+            $0.leading.equalTo(view.safeAreaLayoutGuide.snp.leading).offset(20)
+            $0.top.equalTo(self.view.safeAreaLayoutGuide.snp.top).offset(60)
         }
         profileImage.snp.makeConstraints {
             $0.width.height.equalTo(50)
-            $0.trailing.equalTo(-20)
-            $0.top.equalTo(60)
+            $0.trailing.equalTo(view.safeAreaLayoutGuide.snp.trailing).offset(-20)
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(60)
+        }
+        searchBar.snp.makeConstraints {
+            $0.top.equalTo(vincentLabel.snp.bottom).offset(30)
+            $0.leading.equalTo(view.safeAreaLayoutGuide.snp.leading).offset(15)
+            $0.trailing.equalTo(view.safeAreaLayoutGuide.snp.trailing).offset(-15)
+            $0.bottom.equalTo(searchBar.snp.top).offset(60)
         }
     }
-
+//    override func viewDidLayoutSubviews() {
+//        super.viewDidLayoutSubviews()
+//        collectionView.frame = view.bounds
+//        위의 view bounds로 화면 전체를 그리게 만들어보림
+//    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 8
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: HomeCell.cellID, for: indexPath) as! HomeCell
+        cell.backgroundColor = .blue
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 100, height: 100)
+    }
 }
 
 #if DEBUG
